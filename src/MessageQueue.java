06f5e3cca0efc8eff82aac2e56dc936a9f020bf7
Asddddda <57031673+Asddddda@users.noreply.github.com>
2020-03-01 15:33:00 +0800

    Update MessageQueue.java

--- src/MessageQueue.java
+++ src/MessageQueue.java
@@ -1,4 +1,30 @@
-import java.util.Queue;
-
 public class MessageQueue {
+    //先从最基础的数组试试
+    Message[] queue = new Message[10];
+    static int index = 0;
+
+    public void add(Message message) {
+        queue[index++] = message;
+    }
+
+    public boolean isEmpty() {
+        for (int i = 0; i < index; i++) {
+            if (queue[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Message poll(){
+        Message firstMsg = firstMessage();
+        for(int i = 1; i < index; i++) {
+            queue[i-1] = queue[i];
+        }
+        return firstMsg;
+    }
+
+    public Message firstMessage(){
+        return queue[0];
+    }
 }
